9995ca2945d2456d73b0854f7eca330f2af94d68
João Pedro de Oliveira Nascente <joao.nascente@edu.ufes.br>
2024-02-14 14:36:17 -0300

    correcao de bug em connection.py

--- sheetsManagment/connection.py
+++ sheetsManagment/connection.py
@@ -5,6 +5,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
+
 def Stablishes_connection():
     """
     Establishes connection with the Google Sheets API and returns the required spreadsheet service.
@@ -13,15 +14,12 @@ def Stablishes_connection():
         spreadSheetService: The connected spreadsheet service.
     """
 
-
-#Stablishes connection to the Google sheets api and returns the required spreadsheet service
-def Stablishes_connection():
     SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
     creds = None
 
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists("sheetsManagment/token.json"):
+        creds = Credentials.from_authorized_user_file("sheetsManagment/token.json", SCOPES)
 
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -29,11 +27,11 @@ def Stablishes_connection():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
+                "sheetsManagment/credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", "w") as token:
+        with open("sheetsManagment/token.json", "w") as token:
             token.write(creds.to_json())
 
     service = build("sheets", "v4", credentials=creds)
